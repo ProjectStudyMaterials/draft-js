4f016beee99c420e831017da7116741c8fc2ffa7
andrew <aj.gray619@gmail.com>
2021-04-16 14:43:09 -0700

    enhance package build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./scripts/enforcePreCommitSetup.sh
 echo 'Building...'
-gulp
-echo 'Building...done'
+if gulp
+then
+  echo 'Building...done'
+else
+  echo "Something went wrong when building (see above). Please inspect the code, fix the issue, and try again."
+  exit "$?"
+fi
