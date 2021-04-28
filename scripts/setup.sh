a14e5051b4ed91efb7810c43d95be1a5e8067c32
andrew <aj.gray619@gmail.com>
2021-04-27 20:58:27 -0700

    same message

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,3 @@ preCommitFile=".git/hooks/pre-commit"
 echo "Setting up pre-commit hooks for git..."
 cp "scripts/preCommit.sh" "$preCommitFile"
 echo "Setting up pre-commit hooks for git...done"
-
-# Intentionally not using yarn 2.0 aka berry
-yarn install
